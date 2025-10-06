5833a370652a9eb92674aee98ffdee333d1be5f9
5414R 424M1 <161249294+SalarAzami@users.noreply.github.com>
2025-10-06 11:47:24 +0200

    Update DrawACard.py

--- DrawACard.py
+++ DrawACard.py
@@ -1,8 +1,13 @@
 import random
 
-def draw_card(deck):
-  hand = deck.pop()
-  return [hand], deck
+def draw_card(deck, num_cards):
+  hand = []
+  for _ in range(num_cards):
+    if deck:
+      hand.append(deck.pop())
+    else:
+      break
+  return hand, deck
 
 def create_deck():
   suits = ["♥", "♦", "♣", "♠"]
@@ -16,10 +21,24 @@ def create_deck():
      # print(deck)
   return deck
 
+def show_card(card):
+  space = " "
+  if len(card[1]) == 2:
+    space = ""
+  print (f"""
+  +-------+
+  |{card[1]}     {space}|
+  |       |
+  |   {card[0]}   |
+  |       |
+  |{space}     {card[1]}|
+  +-------+
+  """)
 deck = create_deck()
 while len(deck) > 0:
-  input("Press Enter to draw the next card")
-  hand, deck = draw_card(deck)
-  print(hand[0])
+  num_cards = int(input("How many cards do you want to draw? "))
+  hand, deck = draw_card(deck, num_cards)
+  for card in hand:
+    show_card(card)
 
 print("We are out of cards")
